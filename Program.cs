4af46f7f34b7ea27c4f98154e142aae0f3c370d0
GromDV <gromdv@gmail.com>
2022-11-30 16:39:15 +0300

    Solution uses methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
+// Метод выводит в консоль строковый массив через запятую
 void PrintArray(string[] array)
 {
     Console.Write("[ ");
@@ -11,23 +12,31 @@ void PrintArray(string[] array)
     }
     Console.WriteLine(" ]");
 }
+// Метод считает количество элементов, равных или короче param
+int CountElemsLessThen(string[] array, int param)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= param)
+            counter++;
+    return counter;
+}
+// Метод возвращает массив строк, равных или короче param
+string[] SelectLessStr(string[] array, int param)
+{
+    string[] arrOut = new string[CountElemsLessThen(array, param)];
+    int newCounter = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+            arrOut[newCounter++] = array[i];
+    return arrOut;
+}
 
-string[] arrayIn = { "hello", "2", "world", ":-)" };
 
-PrintArray(arrayIn);
+string[] arrayIn = { "hello", "2", "world", ":-)" };    // Создаем массив разных строк
 
-int counter = 0;
-for (int i = 0; i < arrayIn.Length; i++)
-    if(arrayIn[i].Length <=3)
-        counter++;
-    
-string[] arrOut = new string[counter];
+PrintArray(arrayIn);                                    // Выводим этот массив
 
-int newCounter = 0;
-for (int i = 0; i < arrayIn.Length; i++)
-    if(arrayIn[i].Length <=3)
-        arrOut[newCounter++] = arrayIn[i];
+string[] less3str = SelectLessStr(arrayIn, 3);          // В новый массив отбираем строки, равные или короче 3
 
-PrintArray(arrOut);
-
-
+PrintArray(less3str);                                   // Выводим этот массив
